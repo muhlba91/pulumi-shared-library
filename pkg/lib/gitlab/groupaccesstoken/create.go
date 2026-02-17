f44667bc231245ad4053ddd3c9e1335aeb0f2ff5
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-02-17 17:18:23 +0100

    feat: add gitlab group access tokens

---
+++ pkg/lib/gitlab/groupaccesstoken/create.go
@@ -0,0 +1,43 @@
+package accesstoken
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+
+	"github.com/pulumi/pulumi-gitlab/sdk/v9/go/gitlab"
+)
+
+// CreateOptions defines the options for creating a GitLab group access token.
+type CreateOptions struct {
+	// Name is the name of the group access token.
+	Name pulumi.StringInput
+	// Description is the description of the group access token.
+	Description pulumi.StringInput
+	// Group is the ID or full path of the group for which the access token will be created.
+	Group string
+	// Scopes is a list of scopes to assign to the group access token.
+	Scopes []string
+	// PulumiOptions are additional options to pass to the Pulumi resource.
+	PulumiOptions []pulumi.ResourceOption
+}
+
+// Create creates a new GitLab group access token with the specified options.
+// ctx: The Pulumi context.
+// name: The logical name for the Pulumi resource.
+// opts: The options for creating the group access token.
+func Create(ctx *pulumi.Context, name string, opts *CreateOptions) (*gitlab.GroupAccessToken, error) {
+	scopes := make([]string, 0, len(opts.Scopes)+1)
+	scopes = append(scopes, opts.Scopes...)
+	scopes = append(scopes, "self_rotate")
+	slices.Sort(scopes)
+
+	return gitlab.NewGroupAccessToken(ctx, fmt.Sprintf("gitlab-pat-%s", name), &gitlab.GroupAccessTokenArgs{
+		Name:        opts.Name,
+		Description: opts.Description,
+		Group:       pulumi.String(opts.Group),
+		Scopes:      pulumi.ToStringArray(scopes),
+		AccessLevel: pulumi.String("maintainer"),
+	}, opts.PulumiOptions...)
+}
---
+++ pkg/lib/gitlab/groupaccesstoken/create_test.go
@@ -0,0 +1,78 @@
+package accesstoken_test
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	accesstoken "github.com/muhlba91/pulumi-shared-library/pkg/lib/gitlab/groupaccesstoken"
+	"github.com/muhlba91/pulumi-shared-library/test/mocks"
+)
+
+func TestCreate(t *testing.T) {
+	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
+		group := "test-group"
+		opts := &accesstoken.CreateOptions{
+			Name:        pulumi.String("test-token"),
+			Description: pulumi.String("test description"),
+			Group:       group,
+			Scopes:      []string{"api", "read_user"},
+		}
+
+		r, err := accesstoken.Create(ctx, "test", opts)
+		require.NoError(t, err)
+		require.NotNil(t, r)
+
+		r.Name.ApplyT(func(n string) error {
+			assert.Equal(t, "test-token", n)
+			return nil
+		})
+		r.Description.ApplyT(func(d string) error {
+			assert.Equal(t, "test description", d)
+			return nil
+		})
+		r.Group.ApplyT(func(id string) error {
+			assert.Equal(t, group, id)
+			return nil
+		})
+		r.AccessLevel.ApplyT(func(id string) error {
+			assert.Equal(t, "maintainer", id)
+			return nil
+		})
+		r.Scopes.ApplyT(func(s []string) error {
+			assert.ElementsMatch(t, []string{"api", "read_user", "self_rotate"}, s)
+			return nil
+		})
+
+		return nil
+	}, pulumi.WithMocks("project", "stack", mocks.Mocks(0)))
+	require.NoError(t, err)
+}
+
+func TestCreate_Minimal(t *testing.T) {
+	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
+		group := "test-group"
+		opts := &accesstoken.CreateOptions{
+			Name:  pulumi.String("minimal-token"),
+			Group: group,
+		}
+
+		r, err := accesstoken.Create(ctx, "minimal", opts)
+		require.NoError(t, err)
+		require.NotNil(t, r)
+
+		r.Group.ApplyT(func(id string) error {
+			assert.Equal(t, group, id)
+			return nil
+		})
+		r.Scopes.ApplyT(func(s []string) error {
+			assert.Equal(t, []string{"self_rotate"}, s)
+			return nil
+		})
+
+		return nil
+	}, pulumi.WithMocks("project", "stack", mocks.Mocks(0)))
+	require.NoError(t, err)
+}
