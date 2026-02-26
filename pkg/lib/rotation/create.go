a5a09cd0bf5c40a405146dd75bf124871e61838e
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-02-26 14:21:57 +0100

    refactor: move rotation options to model

--- pkg/lib/rotation/create.go
+++ pkg/lib/rotation/create.go
@@ -5,20 +5,22 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumiverse/pulumi-time/sdk/go/time"
+
+	rModel "github.com/muhlba91/pulumi-shared-library/pkg/model/rotation"
 )
 
 // Create creates a rotating resource.
 // If days is <= 0 it defaults to 30.
 // ctx: Pulumi context.
-// name: Name prefix for the rotation resource.
-// days: Number of days between rotations.
-func Create(ctx *pulumi.Context, name string, days int) (pulumi.CustomResource, error) {
+// opts: Options for creating the rotation resource.
+func Create(ctx *pulumi.Context, opts *rModel.Options) (pulumi.CustomResource, error) {
+	days := opts.Days
 	if days <= 0 {
 		days = 30
 	}
 
 	return time.NewRotating(ctx,
-		fmt.Sprintf("rotation-%s", name),
+		fmt.Sprintf("rotation-%s", opts.Name),
 		&time.RotatingArgs{
 			RotationDays: pulumi.Int(days),
 		},
